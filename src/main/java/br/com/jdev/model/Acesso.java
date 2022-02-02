0af7e2851e2601067ec6b3a5822f2cd0c12fc908
alisoncruz <alisonn83@gmail.com>
2022-02-02 02:13:22 -0300

    criação de classes de modelo, alteração de classes já existentes

--- src/main/java/br/com/jdev/model/Acesso.java
+++ src/main/java/br/com/jdev/model/Acesso.java
@@ -1,19 +1,19 @@
 package br.com.jdev.model;
 
-import java.io.Serializable;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.Column;
+
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1, allocationSize = 1)
+public class Acesso implements GrantedAuthority {
 
 	/**
 	 * 
@@ -21,24 +21,25 @@ public class MarcaProduto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
 	private Long id;
 
 	@Column(nullable = false)
-	private String nomeDesc;
+	private String descricao;
 
-	public String getNomeDesc() {
-		return nomeDesc;
+	@Override
+	public String getAuthority() {
+		return descricao;
 	}
 
-	public void setNomeDesc(String nomeDesc) {
-		this.nomeDesc = nomeDesc;
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -47,8 +48,8 @@ public class MarcaProduto implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nomeDesc == null) ? 0 : nomeDesc.hashCode());
 		return result;
 	}
 
@@ -60,20 +61,18 @@ public class MarcaProduto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
+		Acesso other = (Acesso) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (nomeDesc == null) {
-			if (other.nomeDesc != null)
-				return false;
-		} else if (!nomeDesc.equals(other.nomeDesc))
-			return false;
 		return true;
 	}
-	
-	
 
 }
--- src/main/java/br/com/jdev/model/CategoriaProduto.java
+++ src/main/java/br/com/jdev/model/CategoriaProduto.java
@@ -34,6 +34,10 @@ public class CategoriaProduto implements Serializable {
 	public void setNomeDesc(String nomeDesc) {
 		this.nomeDesc = nomeDesc;
 	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public Long getId() {
 		return id;
